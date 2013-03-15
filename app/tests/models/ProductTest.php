e8db765a6e972867ccf1f36bad0a8d029e94ee3d
Zizaco <zizaco@gmail.com>
2013-03-15 09:08:55 -0300

    [link-products] Products model

---
+++ app/tests/models/ProductTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Zizaco\FactoryMuff\Facade\FactoryMuff as f;
+
+class ProductTest extends TestCase
+{
+    /**
+     * Clean collection between every test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->cleanCollection( 'products' );
+        $this->cleanCollection( 'categories' );
+    }
+
+    /**
+     * Should save valid product
+     *
+     */
+    public function testShouldSaveValidProduct()
+    {
+        $product = new Product;
+
+        $product->name = 'product';
+        $product->category = f::create('Category')->_id;
+
+        // Should return true, since it's a valid product
+        $this->assertTrue( $product->save() );
+    }
+
+    /**
+     * Should NOT save invalid product
+     *
+     */
+    public function testShouldNotSaveInvalidProduct()
+    {
+        $product = new Product;
+
+        $product->name = '';
+        $product->category = f::create('Category')->_id;
+
+        // Should return true, since it's a valid product
+        $this->assertFalse( $product->save() );
+    }
+
+    /**
+     * Should not set attributes that are not
+     * especified in massAssignment.
+     *
+     */
+    public function testShouldNotSetAnyAttributes()
+    {
+        $document = new Product;
+
+        $input = array(
+            'id' => 123123,
+            'name' => 'thename',
+            'nonexistant' => 'avalue',
+            'category' => f::create('Category')->_id,
+        );
+
+        $document->fillable = array('name','category');
+
+        $document->fill( $input );
+
+        $this->assertNotEquals( $input['id'], $document->getAttribute('id') );
+        $this->assertEquals   ( $input['name'], $document->getAttribute('name') );
+        $this->assertNotEquals( $input['nonexistant'], $document->getAttribute('nonexistant') );
+        $this->assertEquals   ( $input['category'], $document->getAttribute('category') );
+    }
+}
