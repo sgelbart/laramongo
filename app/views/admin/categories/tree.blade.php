0bfa6c4ba90c5285a1978a46a2b4582a5b9b0b11
Zizaco <zizaco@gmail.com>
2013-03-20 17:23:45 -0300

    [category-tree-as-default] New category button

--- app/views/admin/categories/tree.blade.php
+++ app/views/admin/categories/tree.blade.php
@@ -2,6 +2,14 @@
 
     <h2>Categorias</h2>
 
+    <p>
+        <a href='{{ URL::action( 'Admin\CategoriesController@create' ) }}' class='btn btn-primary' id='btn-create-new-category'>
+            Nova Categoria
+        </a>
+    </p>
+
+    <hr>
+
     <div class='tree' data-tree='true'>
         {{ Category::showTree() }}
     </div>
