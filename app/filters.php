bdfbcd551aa2efc860c844534a7de4d02e193189
Zizaco <zizaco@gmail.com>
2013-03-27 09:27:44 -0300

    Recent laravel4 changes

--- app/filters.php
+++ app/filters.php
@@ -57,8 +57,8 @@ Route::filter('guest', function()
 
 Route::filter('csrf', function()
 {
-	if (Session::getToken() != Input::get('csrf_token'))
-	{
-		throw new Illuminate\Session\TokenMismatchException;
-	}
+	if (Session::getToken() != Input::get('_token'))
+    {
+        throw new Illuminate\Session\TokenMismatchException;
+    }
 });
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -63,7 +63,9 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-require $app->getBootstrapFile();
+$framework = __DIR__.'/../vendor/laravel/framework/src';
+
+require $framework.'/Illuminate/Foundation/start.php';
 
 /*
 |--------------------------------------------------------------------------
