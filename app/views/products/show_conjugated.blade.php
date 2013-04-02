521000472fcaae17e602fb0175533df3924488f6
Zizaco <zizaco@gmail.com>
2013-04-02 10:50:56 -0300

    [conjugated-product] Conjugated product show

--- app/views/products/show_conjugated.blade.php
+++ app/views/products/show_conjugated.blade.php
@@ -14,7 +14,12 @@
 
         <p>{{ $product->description }}</p>
 
+        @include('products._product_characteristics')
+
+        <h2>Composto por</h2>
         @foreach($conjProducts as $product)
+
+            <h3>{{ Html::linkAction('ProductsController@show', $product->name, ['id'=>$product->_id] ) }}</h3>
 
             <?php $category = $product->category(); ?>
             @include('products._product_characteristics')
