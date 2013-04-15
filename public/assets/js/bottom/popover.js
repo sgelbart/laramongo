8e75057f6a8c8d4f1ccb37ce84d2689224b3e1cf
Zizaco <zizaco@gmail.com>
2013-04-15 18:23:49 -0300

    [product-popover] Popover.js refactoring

--- public/assets/js/bottom/popover.js
+++ public/assets/js/bottom/popover.js
@@ -1,18 +1,47 @@
+// popover.js
+
+/**
+ * For every element containing the 'data-with-popover' attribute
+ * on hover, position and display the .popover div within it
+ *
+ * Ex:
+ *   <spam data-with-popover>
+ *       <h1>Hover me!</h1>
+ *       <div class='popover'>
+ *           <p>This is the popover</p>
+ *       </div>
+ *   </span>
+ *
+ * PS: It's a good idea to use it with the "ToPopover" trait ;)
+ * 
+ */
+
 var Popover = function() {
-  this.init = function() {
-    $('span[data-with-popover]').hover(function() {
-      var x = $(this).offset().left;
-      var y = $(this).offset().top;
-      var height_popover = $(this).find('.popover').height();
 
-      $(this).find('.popover').css('left', x).css('top', y - height_popover).show();
+    this.init = function() {
+        $('[data-with-popover]').hover(function() {
+            var el = $(this);
+            var popover = el.find('.popover');
 
-    }, function() {
-      $(this).find('.popover').hide();
-    });
-  };
+            var x = el.offset().left;
+            var y = el.offset().top;
 
-  this.init();
+            popover
+                .css('left', x - popover.width()/2 + el.width()/2)
+                .css('top', y - popover.height())
+                .show();
+
+        }, function() {
+            var el = $(this);
+
+            el.find('.popover').hide();
+        });
+    };
+
+    this.init();
 };
 
-new Popover();
+$(function(){
+    new Popover();    
+});
+
