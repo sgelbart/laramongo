72261573d86d5e29ceb9ae6f646601f01b1769d5
Zizaco <zizaco@gmail.com>
2013-04-18 15:07:08 -0300

    [image-tagging] Image tagging acceptance test

--- app/views/admin/contents/_image_tag_form.blade.php
+++ app/views/admin/contents/_image_tag_form.blade.php
@@ -14,7 +14,7 @@
     {{ Form::label('product_id', 'Product', ['class'=>'control-label']) }}
     {{ Form::select('product_id', $productsOption, false, ['data-chosen'=>'true'] ) }}
 
-    {{ Form::button(l('content.save_tag_image'), ['type'=>'submit', 'id'=>'submit-save-product', 'class'=>'btn btn-primary'] ) }}
+    {{ Form::button(l('content.save_tag_image'), ['type'=>'submit', 'id'=>'submit-tag-image', 'class'=>'btn btn-primary'] ) }}
     <a class='btn' data-close-popover='true'>Cancelar</a>
     <a class='btn btn-danger delete-tag' href='{{ URL::action('Admin\ContentsController@untagProduct',['id'=>$content->_id, 'tag_id'=>0]) }}' data-method='DELETE'>
         <i class='icon-trash icon-white'></i>
