d2675f52a7ac949e0136369ada29fa197aa16812
Guilherme Guitte <guilherme.guitte@gmail.com>
2013-04-18 10:50:20 -0300

    [layout-views] basic layout responsive

--- app/views/templates/responsive/categories/show.blade.php
+++ app/views/templates/responsive/categories/show.blade.php
@@ -1,3 +1,37 @@
 @section('content')
-ssadds
+    <p>
+        Seu caminho foi:
+        {{ Html::link( '/', 'Home' ) }} >
+        {{ $category->name }}
+    </p>
+
+    <div class="family">
+      <h2>Family of {{ ucfirst($category->name) }}</h2>
+        <div class='photo'>
+          <img src="{{ $category->imageUrl() }}">
+        </div>
+
+        <div class="description">
+          <h3>{{ ucfirst($category->name) }}</h3>
+          {{ $category->description }}
+      </div>
+    </div>
+
+
+    <ul class="thumbnails" id="products-images">
+        @foreach ($products as $product)
+          <li class='responsive'>
+            <div class="thumbnail" >
+                <img src="{{ $product->imageUrl() }}" alt="">
+
+              <div class="caption">
+                <p><b>Nome:</b> {{ $product->name }}</p>
+                <p><b>Descrição:</b> {{ $product->description }}</p>
+
+                <a href="/product/{{$product->_id}}" class="button button-block">See more</a>
+              </div>
+            </div>
+          </li>
+        @endforeach
+    </ul>
 @stop
