e225ea2dde32fe5ef7d2667e4d6e807bd36a9270
Guilherme Guitte <guilherme.guitte@gmail.com>
2013-04-29 10:43:10 -0300

    [frontend-fixs] changing product photos

--- app/views/templates/base/categories/_products.blade.php
+++ app/views/templates/base/categories/_products.blade.php
@@ -2,7 +2,7 @@
     <a href='{{ URL::action('ProductsController@show', ['id'=>$product->_id]) }}'>
         <div class='product_tile'>
             <h3>{{ $product->name }}</h3>
-            <img style='background-image: url({{ $product->imageUrl() }})'>
+            <img style='background-image: url({{ $product->imageUrl(3) }})'>
         </div>
     </a>
 @endforeach
