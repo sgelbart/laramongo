cccc7565090b17172493817fe4a8b86cc9823f08
Zizaco <zizaco@gmail.com>
2013-04-29 10:39:20 -0300

    [google-map-in-shop] Display google-maps image in shop content

--- app/views/templates/base/contents/show_shop.blade.php
+++ app/views/templates/base/contents/show_shop.blade.php
@@ -1,8 +1,11 @@
 @section('content')
     <h1>Loja <b>{{ $content->name }}</b></h1>
     <h2>Endereço: <b>{{ $content->address }}</b></h2>
-    <h2>CEP: <b>{{ $content->cep }}</b></h2>
-    <h2>Fone(s): <b>{{ $content->phones }}</b></h2>
-    <h2>Informações: <b>{{ $content->description }}</b></h2>
+
+    <a href="{{ $content->address }}" target="blank"><img alt="Staticmap?size=350x255&amp;center={{ $content->address }}&amp;zoom=15&amp;sensor=false&amp;markers=color:green|{{ $content->address }}," src="https://maps.google.com/maps/api/staticmap?size=700x400&amp;center={{ $content->address }},&amp;zoom=15&amp;sensor=false&amp;markers=color:green|{{ $content->address }},"></a>
+
+    <h2>CEP:</h2> {{ $content->cep }}
+    <h2>Fone(s):</h2> {{ $content->phones }}
+    <h2>Informações:</h2> {{ $content->description }}
 
 @stop
