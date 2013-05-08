87ff6e35c735192fa0c112596f06b0461e1a9b4e
Guilherme Guitte <guilherme.guitte@gmail.com>
2013-05-08 09:07:40 -0300

    [elastic-search] added acceptance test at search engine

--- app/tests/acceptance/contexts/IndexResourcesContext.php
+++ app/tests/acceptance/contexts/IndexResourcesContext.php
@@ -7,7 +7,32 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
+use Laramongo\SearchEngine\ElasticSearchEngine;
+
+use Mockery as m;
+
 class IndexResourcesContext extends BaseContext {
 
-    public function __construct() { }
+     /**
+     * @Then /^should have indexed product at Search Engine$/
+     */
+    public function shouldHaveIndexedProductAtSearchEngine()
+    {
+        Config::set('search_engine.enabled', true);
+
+        // Prepare mocked searchEngine
+        $this->mockedEs = m::mock('Es');
+        $this->mockedEs->shouldReceive('setIndex');
+        $this->mockedEs->shouldReceive('setType');
+
+        $this->mockedEs
+            ->shouldReceive('index')
+            ->with($this->simple_valid_product->getAttributes(), $this->simple_valid_product->_id)
+            ->once();
+
+        $searchEngine = new ElasticSearchEngine;
+        $searchEngine->es = $this->mockedEs;
+
+        $searchEngine->indexObject($this->simple_valid_product);
+    }
 }
